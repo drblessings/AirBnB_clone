619672a0cb1c1cf227df33f62f256131b8c6f07b
drblessings <oladunjoyebabafemi@gmail.com>
2023-02-10 05:34:17 -0800

    Task 9

---
+++ models/amenity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+class Amenity inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Public atrribute is name
+    it is an empty string
+    """
+
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+class State inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Public attribute is name
+    it is an empty string
+    """
+
+    name = ""
